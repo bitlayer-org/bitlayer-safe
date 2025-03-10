941919acb639c1768c8b370e3ab2583f84ab0b2d
mason <15539588217@163.com>
2025-03-10 14:48:39 +0800

    feat: first 1.52.2

--- src/services/transactions/index.tests.ts
+++
@@ -1,8 +0,0 @@
-import { getTimezoneOffset } from '.'
-
-describe('getTimezoneOffset', () => {
-  it('should return timezone offset in milliseconds', () => {
-    const CET = 60 * 60 * 1000 // tests are run in CET
-    expect(getTimezoneOffset()).toBe(-CET)
-  })
-})
