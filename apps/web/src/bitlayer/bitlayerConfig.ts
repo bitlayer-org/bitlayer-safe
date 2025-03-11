2aabf3152b8c1a85bc2b23d18ae9b3a3e672dd8d
mason <15539588217@163.com>
2025-03-11 14:12:29 +0800

    feat: update config

--- apps/web/src/bitlayer/bitlayerConfig.ts
+++ apps/web/src/bitlayer/bitlayerConfig.ts
@@ -2,41 +2,41 @@ import { FEATURES } from '@safe-global/safe-gateway-typescript-sdk'
 
 export const bitlayerConfig = {
     "balancesProvider":{
-      "chainName": 'bitlayer',
+      "chainName": 'bitlayer-test',
       "enabled": false
     },
-    "chainId": "200901",
-    "chainName": "Bitlayer Mainnet",
+    "chainId": "200810",
+    "chainName": "Bitlayer Testnet",
     "shortName": "BTR",
-    "description": "Bitlayer Mainnet",
+    "description": "Bitlayer Testnet",
     "chainLogoUri": "https://multisign.bitlayer.org/cfg/media/chains/200901/chain_logo.png",
     "l2": true,
-    "isTestnet": false,
+    "isTestnet": true,
     "rpcUri": {
         "authentication": "NO_AUTHENTICATION",
-        "value": "https://rpc.bitlayer.org"
+        "value": "https://testnet-rpc.bitlayer.org"
     },
     "safeAppsRpcUri": {
         "authentication": "NO_AUTHENTICATION",
-        "value": "https://rpc.bitlayer.org"
+        "value": "https://testnet-rpc.bitlayer.org"
     },
     "publicRpcUri": {
         "authentication": "NO_AUTHENTICATION",
-        "value": "https://rpc.bitlayer.org"
+        "value": "https://testnet-rpc.bitlayer.org"
     },
     "blockExplorerUriTemplate": {
-        "address": "https://www.btrscan.com/address/{{address}}",
-        "txHash": "https://www.btrscan.com/tx/{{txHash}}",
-        "api": "https://api.btrscan.com/scan/api"
+        "address": "https://testnet.btrscan.com/address/{{address}}",
+        "txHash": "https://testnet.btrscan.com/tx/{{txHash}}",
+        "api": "https://api-testnet.btrscan.com/scan/api"
     },
     "nativeCurrency": {
         "name": "Bitcoin",
         "symbol": "BTC",
         "decimals": 18,
-        "logoUri": "https://multisign.bitlayer.org/cfg/media/chains/200901/currency_logo.png"
+        "logoUri": "https://test-multisign.bitlayer.org/cfg/media/chains/200810/currency_logo.png"
     },
-    "transactionService": "https://multisign.bitlayer.org/txs",
-    "vpcTransactionService": "https://multisign.bitlayer.org/txs",
+    "transactionService": "https://test-multisign.bitlayer.org/txs",
+    "vpcTransactionService": "https://test-multisign.bitlayer.org/txs",
     "theme": {
         "textColor": "#ffffff",
         "backgroundColor": "#000000"
